567ee8a04db67c64d71d937a9970313e5c415dac
Jepi Oktamipa <jefyokta50@gmail.com>
2025-04-24 08:07:32 +0700

    file watcher

--- idehelper.php
+++ idehelper.php
@@ -2,24 +2,10 @@
 
 class FFI
 {
-    /**
-     * Jalankan fungsi C async (via FFI)
-     *
-     * @param string $name
-     * @param int $delay
-     * @return AsyncTask
-     */
     public function run_async(string $name, int $delay): AsyncTask {
 
         return new AsyncTask;
     }
-
-    /**
-     * Cek status task async
-     *
-     * @param AsyncTask $task
-     * @return int
-     */
     public function task_done(AsyncTask $task): int {
         return 1 ?? 0;
     }
